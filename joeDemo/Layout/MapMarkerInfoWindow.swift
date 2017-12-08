4fb25122de27bce6952a141656231aa0d60e8831
User <namansharma@live.ca>
2017-12-08 15:52:42 -0800

    personal

--- joeDemo/Layout/MapMarkerInfoWindow.swift
+++ joeDemo/Layout/MapMarkerInfoWindow.swift
@@ -12,15 +12,7 @@ class MapMarkerInfoWindow: UIView {
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var text: UILabel!
-    @IBOutlet weak var message: UIButton!
-    @IBOutlet weak var profile: UIButton!
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+   
     
     class func instanceFromNib() -> UIView {
         return UINib(nibName: "MapMarkerInfoWindow", bundle: nil).instantiate(withOwner: self, options: nil).first as! UIView
